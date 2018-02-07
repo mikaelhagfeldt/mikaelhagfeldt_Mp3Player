9ccfbd34c4772a00135bfd75c21baa9a1786a02f
mikaelhagfeldt <33085380+mikaelhagfeldt@users.noreply.github.com>
2018-02-07 12:19:06 +0100

    Förberedelser. Förbereder för att kunna se min lista av låtar.

--- app/src/main/java/com/example/mikael/mikaelhagfeldt_mp3player/MainActivity.java
+++ app/src/main/java/com/example/mikael/mikaelhagfeldt_mp3player/MainActivity.java
@@ -2,14 +2,24 @@ package com.example.mikael.mikaelhagfeldt_mp3player;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
+    private ListView fieldListView;                                                                 // Musiken kommer att synas här
+    private ArrayList<Music> fieldArrayList;                                                        // Musiken läggs in här
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.fieldListView = findViewById(R.id.id_listView);
+        this.fieldArrayList = new ArrayList<>();
+
+
     }
 }
