1c74dece8f08da32534c996b532ae06ed634002b
mikaelhagfeldt <33085380+mikaelhagfeldt@users.noreply.github.com>
2018-02-07 16:58:47 +0100

    Låtlista. Koden fungerar nu, men ingenting syns fortfarande på skärmen.

--- app/src/main/java/com/example/mikael/mikaelhagfeldt_mp3player/MainActivity.java
+++ app/src/main/java/com/example/mikael/mikaelhagfeldt_mp3player/MainActivity.java
@@ -29,7 +29,8 @@ public class MainActivity extends AppCompatActivity
 
     /*
         En metod för att hämta musikfilerna med ContentResolver, och använda Cursor
-        för att komma åt dem.
+        för att komma åt dem. Uppenbarligen försöker man komma åt data som finns i kolumner först,
+        innan man lägger till detta i en arraylist.
      */
 
     public void getMusic()
@@ -37,5 +38,23 @@ public class MainActivity extends AppCompatActivity
         ContentResolver contentResolver = getContentResolver();                                     // Från Developer Android
         Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
         Cursor cursor = contentResolver.query(uri, null, null, null, null);
+
+        if (cursor.moveToFirst() && cursor != null)
+        {
+
+            int intColumn1 = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);                   // Hämtar data från kolumner
+            int intColumn2 = cursor.getColumnIndex(MediaStore.Audio.Media._ID);
+            int intColumn3 = cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+
+            do
+            {
+                int intID = cursor.getInt(intColumn2);                                              // Lägger till musik i arraylist
+                String strTitle = cursor.getString(intColumn1);
+                String strArtist = cursor.getString(intColumn3);
+                Music newMusic = new Music(strTitle, strArtist, intID);
+                this.fieldArrayList.add(newMusic);
+            }
+            while (cursor.moveToNext());
+        }
     }
 }
