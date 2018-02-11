86df7274696febb891033fcebc228daeb4a89cef
mikaelhagfeldt <33085380+mikaelhagfeldt@users.noreply.github.com>
2018-02-11 10:54:10 +0100

    Klass Adapter. Anledningen att jag inte har sett någonting på skärmen tycks ha och göra med att jag inte har någon adapter som kan överföra datan i en lista till en View.

---
+++ app/src/main/java/com/example/mikael/mikaelhagfeldt_mp3player/Adapter.java
@@ -0,0 +1,41 @@
+package com.example.mikael.mikaelhagfeldt_mp3player;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by mikael on 2018-02-11.
+ */
+
+/*
+    En klass som ansvarar för att skapa en "View" för varje element i en lista.
+ */
+
+public class Adapter extends BaseAdapter
+{
+
+    @Override
+    public int getCount()
+    {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i)
+    {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i)
+    {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup)
+    {
+        return null;
+    }
+}
