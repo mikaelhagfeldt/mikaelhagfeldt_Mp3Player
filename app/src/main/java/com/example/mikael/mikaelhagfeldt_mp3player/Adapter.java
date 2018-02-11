2be00ecba3b97897ed8075fe1e309c34046d0608
mikaelhagfeldt <33085380+mikaelhagfeldt@users.noreply.github.com>
2018-02-11 11:13:33 +0100

    Klass Adapter. Kan inte finna "parent". Någonting fel i kontexten?

--- app/src/main/java/com/example/mikael/mikaelhagfeldt_mp3player/Adapter.java
+++ app/src/main/java/com/example/mikael/mikaelhagfeldt_mp3player/Adapter.java
@@ -1,8 +1,14 @@
 package com.example.mikael.mikaelhagfeldt_mp3player;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by mikael on 2018-02-11.
@@ -14,11 +20,19 @@ import android.widget.BaseAdapter;
 
 public class Adapter extends BaseAdapter
 {
+    private ArrayList<Music> fieldMusicArrayList;
+    private LayoutInflater fieldLayoutInflator;                                                     // Omvandlar XML layout fil till View objekt.
+
+    public Adapter(Context paramContext, ArrayList<Music> paramList)
+    {
+        this.fieldMusicArrayList = paramList;
+        this.fieldLayoutInflator = LayoutInflater.from(paramContext);
+    }
 
     @Override
     public int getCount()
     {
-        return 0;
+        return this.fieldMusicArrayList.size();
     }
 
     @Override
@@ -36,6 +50,8 @@ public class Adapter extends BaseAdapter
     @Override
     public View getView(int i, View view, ViewGroup viewGroup)
     {
-        return null;
+        LinearLayout localLinearLayout = (LinearLayout)fieldLayoutInflator.inflate(R.layout.music, parent, false);
+        TextView localTextViewName = localLinearLayout.findViewById(R.id.id_musicTitle);
+        TextView localTextViewArtist =
     }
 }
